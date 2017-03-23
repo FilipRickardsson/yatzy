2ef90c478084b91bd513d1873d1a0fe66d82504b
leogard shatri <leogards@gmail.com>
2017-03-23 15:36:19 +0100

    DOM-manipulation dice switch
    switch dice on random, shows in DOM.

--- js/classes/die.class.js
+++ js/classes/die.class.js
@@ -8,26 +8,27 @@ class Die extends Base {
         this.dots = Math.floor(Math.random() * 6) + 1;
 
         if (this.dots === 1) {
-            setUtfDots(U + 2680);
+            this.setUtfDots("\u2680");
         }
         if (this.dots === 2) {
-
+            this.setUtfDots("\u2681");
         }
         if (this.dots === 3) {
-
+            this.setUtfDots("\u2682");
         }
         if (this.dots === 4) {
-
+            this.setUtfDots("\u2683");
         }
         if (this.dots === 5) {
-
+            this.setUtfDots("\u2684");
         }
         if (this.dots === 6) {
-
+            this.setUtfDots("\u2685");
         }
+        console.log(this.dots);
     }
     setUtfDots(utfCode) {
-        $('die' + this.id).html(utfCode);
+        $('#die' + this.id).html(utfCode);
 
     }
 }
