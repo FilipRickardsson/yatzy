7e47ab039df65ca89898f823964836d525d7e55f
Dhiaaeddinanabtawi <dhiaaeddin.anabtawi@gmail.com>
2017-03-23 15:51:49 +0100

    Input fields
    Adding input fields depending on user's choice

--- js/classes/lobby.class.js
+++ js/classes/lobby.class.js
@@ -3,4 +3,17 @@ class Lobby extends Base {
 	constructor(propertyValues) {
 		super(propertyValues);
 	}
+
+	groupSize(){
+		var selectedChoice=selectPlayer.selectedIndex;
+		console.log(selectedChoice);
+		$(".inputFields").empty();
+		for(let i=0; i<selectedChoice; i++){
+		$(".inputFields").append(`
+			<div class="input-group">
+			<input type="text" class="form-control" placeholder="Player: " aria-describedby="basic-addon1" >
+			</div>`
+			);
+	}
+}
 }
