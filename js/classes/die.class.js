fe9c4c92baeb6300ca53d529aec9fa35e5a86cb0
Filip <tjo>
2017-03-24 09:34:53 +0100

    replaced if-statements in rollTheDice removed setUtfDots function added getDots function

--- js/classes/die.class.js
+++ js/classes/die.class.js
@@ -5,30 +5,15 @@ class Die extends Base {
     }
 
     rollTheDice() {
-        this.dots = Math.floor(Math.random() * 6) + 1;
+        var dieFaces = ["\u2680", "\u2681", "\u2682", "\u2683", "\u2684", "\u2685"];
+        this.dots = Math.floor(Math.random() * 6 + 1);
+        $('#die' + this.id).html(dieFaces[this.dots - 1]);
 
-        if (this.dots === 1) {
-            this.setUtfDots("\u2680");
-        }
-        if (this.dots === 2) {
-            this.setUtfDots("\u2681");
-        }
-        if (this.dots === 3) {
-            this.setUtfDots("\u2682");
-        }
-        if (this.dots === 4) {
-            this.setUtfDots("\u2683");
-        }
-        if (this.dots === 5) {
-            this.setUtfDots("\u2684");
-        }
-        if (this.dots === 6) {
-            this.setUtfDots("\u2685");
-        }
         console.log(this.dots);
     }
-    setUtfDots(utfCode) {
-        $('#die' + this.id).html(utfCode);
 
+    getDots() {
+        return this.dots;
     }
+    
 }
