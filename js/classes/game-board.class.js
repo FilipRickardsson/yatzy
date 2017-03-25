1bbe13c88d6534c11ea8e15f22540cf3de220a14
Filip <tjo>
2017-03-25 19:14:31 +0100

    added function to calc first half points

--- js/classes/game-board.class.js
+++ js/classes/game-board.class.js
@@ -19,9 +19,31 @@ class GameBoard extends Base {
 	}
 
 	rollTheDice() {
-		for(let i = 0; i < this.dice.length; i++) {
+		for (let i = 0; i < this.dice.length; i++) {
 			this.dice[i].rollTheDice();
 		}
+		this.calcPotentialPoints();
+	}
+
+	calcPotentialPoints() {
+		var points = [];
+		points = points.concat(this.calcFirstHalf());
+		console.log('Points: ', points);
+	}
+
+	calcFirstHalf() {
+		var firstHalfPoints = [];
+		var points = 0;
+		for (let i = 0; i < 6; i++) {
+			for (let j = 0; j < this.dice.length; j++) {
+				if (this.dice[j].getDots() == i + 1) {
+					points += this.dice[j].getDots();
+				}
+			}
+			firstHalfPoints.push(points);
+			points = 0;
+		}
+		return firstHalfPoints;
 	}
 
 }
