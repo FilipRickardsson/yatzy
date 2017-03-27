2b49d7d59992d0190fcb5946ab8a740c8f2a7366
Fatemeh Farmahini <m_farmahini@yahoo.com>
2017-03-27 13:43:37 +0200

    games statistic

--- js/classes/games-result-list.class.js
+++ js/classes/games-result-list.class.js
@@ -15,10 +15,12 @@ class GamesResultList extends List {
 	static get sqlQueries() {
 		return {
 			readGamesResult: `
-				SELECT id ,  DATE_FORMAT(date, '%y-%m-%d') date 
+				SELECT DATE_FORMAT(date, '%y-%m-%d') date , games_id , username 
 				FROM games
-				
-      	
+				JOIN games_has_players
+				ON games.id = games_id
+				JOIN players 
+				ON players_id = players.id
 
 				
 				
