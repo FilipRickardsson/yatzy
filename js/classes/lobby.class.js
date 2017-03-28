41dbbdd81715eb48faf2d472d1ea94a50707137b
leogard shatri <leogards@gmail.com>
2017-03-28 15:25:41 +0200

    TESTresultToDB
    skrivit test data till DB. fungerar. gameBoard.scorePlayers(); tillagd i StartGame(). 
    Och sql satserna finns i scorePlayers() metoden. SÅ värdena läggs in när vi trycker på startGame.

--- js/classes/lobby.class.js
+++ js/classes/lobby.class.js
@@ -1,49 +1,52 @@
 class Lobby extends Base {
 
-	constructor(propertyValues) {
-		super(propertyValues);
-	}
+    constructor(propertyValues) {
+        super(propertyValues);
+    }
 
-	groupSize() {
-		var selectedChoice = $('#selectPlayer').find(":selected").text();
-		console.log('selectedChoice:', selectedChoice);
-		$(".inputFields").empty();
-		for (let i = 0; i < selectedChoice; i++) {
-			$(".inputFields").append(`
+    groupSize() {
+        var selectedChoice = $('#selectPlayer').find(":selected").text();
+        console.log('selectedChoice:', selectedChoice);
+        $(".inputFields").empty();
+        for (let i = 0; i < selectedChoice; i++) {
+            $(".inputFields").append(`
 			     <div class="input-group">
 			         <input type="text" class="form-control" placeholder="Player: " aria-describedby="basic-addon1" >
 			     </div>`);
-		}
-		$('.error').addClass('hidden');
-	}
+        }
+        $('.error').addClass('hidden');
+    }
 
-	startGame() {
-		var temp = this.checkInputFields();
-		console.log(temp);
-		if (temp) {
-			$('body').empty();
-			var gameBoard = new GameBoard();
-			gameBoard.display('body');
-			gameBoard.createDice();
-			gameBoard.createProtocol();
-		} else {
-			$('.error').removeClass('hidden');
-		}
-	}
+    startGame() {
+        var temp = this.checkInputFields();
+        console.log(temp);
 
-	showStatistics() {
-		$('body').empty();
-		var statistics = new Statistics();
-		statistics.display('body');
-	}
+        if (temp) {
+            $('body').empty();
+            var gameBoard = new GameBoard();
+            gameBoard.display('body');
+            gameBoard.createDice();
+            gameBoard.createProtocol();
+            gameBoard.scorePlayers();
+            var testArr = [1, 1337, 250];
+        } else {
+            $('.error').removeClass('hidden');
+        }
+    }
 
-	checkInputFields() {
-		var filled = true;
-		$('input').each(function () {
-			if ($(this).val().length === 0) {
-				filled = false;
-			}
-		});
-		return filled;
-	}
+    showStatistics() {
+        $('body').empty();
+        var statistics = new Statistics();
+        statistics.display('body');
+    }
+
+    checkInputFields() {
+        var filled = true;
+        $('input').each(function () {
+            if ($(this).val().length === 0) {
+                filled = false;
+            }
+        });
+        return filled;
+    }
 }
