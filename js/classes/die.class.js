a9ed67aca5b579f1b8f980455358e5577cc51146
Dhiaaeddinanabtawi <dhiaaeddin.anabtawi@gmail.com>
2017-04-04 09:58:57 +0200

    lockingTheDice
    Now it is not possible to lock dice without rolling them at least once

--- js/classes/die.class.js
+++ js/classes/die.class.js
@@ -5,6 +5,7 @@ class Die extends Base {
 		this.enabled = true;
 		this.dots = "\u2680";
 		this.dieFaces = ["\u2680", "\u2681", "\u2682", "\u2683", "\u2684", "\u2685"];
+		this.counter=0;
 	}
 
 	rollTheDice() {
@@ -12,6 +13,7 @@ class Die extends Base {
 			this.dots = Math.floor(Math.random() * 6 + 1);
 			$('#die' + this.id).html(this.dieFaces[this.dots - 1]);
 		}
+		this.counter++; //Calculate how many times we roll the dice to make it impossible to lock dice without rolling at least once
 	}
 
 	getDots() {
@@ -19,9 +21,14 @@ class Die extends Base {
 	}
 
 	toggleDie() {
+		if (this.counter===0) {
+		console.log('You have to roll at least once!!');
+		 $('body').append('<div style="color: red">You have to roll the dice at least once!!<div>'); 
+		}
+		else{
 		this.enabled = !this.enabled;
 		$('.die' + this.id).toggleClass("dieDisabled");
-
+		}
 	}
 
 }
