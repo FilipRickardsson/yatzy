29c2920f96c317e9b777c874f70a479a6391406b
Tobias <tobias.hjertelundh@hotmail.com>
2017-04-04 13:06:17 +0100

    scores stay in table

--- js/classes/game-board.class.js
+++ js/classes/game-board.class.js
@@ -1,227 +1,227 @@
 class GameBoard extends Base {
 
-	constructor(propertyValues) {
-		super(propertyValues);
-		
-		this.currentPlayer = 0;
-	}
+    constructor(propertyValues) {
+        super(propertyValues);
 
-	createDice() {
-		this.dice = [];
-		for (let i = 0; i < 5; i++) {
-			let die = new Die({
-				id: i + 1
-			});
-			die.display('#dice');
-			this.dice.push(die);
-		}
-	}
+        this.currentPlayer = 0;
+    }
 
-	rollTheDice() {
-		for (let i = 0; i < this.dice.length; i++) {
-			this.dice[i].rollTheDice();
-		}
-		var points = this.calcPotentialPoints();
-		this.protocol.insertPotentialPoints(points, this.currentPlayer);
-		this.switchPlayer();
-	}
+    createDice() {
+        this.dice = [];
+        for (let i = 0; i < 5; i++) {
+            let die = new Die({
+                id: i + 1
+            });
+            die.display('#dice');
+            this.dice.push(die);
+        }
+    }
 
-	createProtocol() {
-		var protocol = new Protocol({
-			players: this.players
-		});
-		this.protocol = protocol;
-		protocol.display('#protocolContainer');
-		protocol.createColumn();
-	}
+    rollTheDice() {
+        for (let i = 0; i < this.dice.length; i++) {
+            this.dice[i].rollTheDice();
+        }
+        var points = this.calcPotentialPoints();
+        this.protocol.insertPotentialPoints(points, this.currentPlayer);
+        this.switchPlayer();
+    }
 
-	calcPotentialPoints() {
-		var points = [];
-		points = points.concat(this.calcFirstHalf());
-		points.push(this.checkOccurences(2));
-		points.push(this.checkDoublePair());
-		points.push(this.checkOccurences(3));
-		points.push(this.checkOccurences(4));
-		points.push(this.checkStraight(true));
-		points.push(this.checkStraight(false));
-		points.push(this.checkFullHouse());
-		points.push(this.calcChance());
-		points.push(this.checkYatzy());
+    createProtocol() {
+        var protocol = new Protocol({
+            players: this.players
+        });
+        this.protocol = protocol;
+        protocol.display('#protocolContainer');
+        protocol.createColumn();
+    }
 
-		points.splice(6, 0, "Not implemented");
-		points.splice(7, 0, "Not implemented");
-		points.push("Not implemented");
-		
-		console.log('Points: ', points);
-		return points;
-	}
+    calcPotentialPoints() {
+        var points = [];
+        points = points.concat(this.calcFirstHalf());
+        points.push(this.checkOccurences(2));
+        points.push(this.checkDoublePair());
+        points.push(this.checkOccurences(3));
+        points.push(this.checkOccurences(4));
+        points.push(this.checkStraight(true));
+        points.push(this.checkStraight(false));
+        points.push(this.checkFullHouse());
+        points.push(this.calcChance());
+        points.push(this.checkYatzy());
 
-	calcFirstHalf() {
-		var firstHalfPoints = [];
-		var points = 0;
-		for (let i = 0; i < 6; i++) {
-			for (let j = 0; j < this.dice.length; j++) {
-				if (this.dice[j].getDots() == i + 1) {
-					points += this.dice[j].getDots();
-				}
-			}
-			firstHalfPoints.push(points);
-			points = 0;
-		}
-		return firstHalfPoints;
-	}
+        points.splice(6, 0, "Not implemented");
+        points.splice(7, 0, "Not implemented");
+        points.push("Not implemented");
 
-	checkOccurences(nbrOfOccurences) {
-		var points = 0;
-		var occurences = 0;
-		for (let i = 6; i > 0; i--) {
-			for (let j = 0; j < this.dice.length; j++) {
-				if (this.dice[j].getDots() == i) {
-					occurences++;
-				}
-			}
-			if (occurences >= nbrOfOccurences) {
-				points = i * nbrOfOccurences;
-				break;
-			}
-			occurences = 0;
-		}
-		return points;
-	}
+        console.log('Points: ', points);
+        return points;
+    }
 
-	checkDoublePair() {
-		var occurences = [0, 0, 0, 0, 0, 0];
-		var pair1, pair2;
-		for (let i = 0; i < this.dice.length; i++) {
-			occurences[this.dice[i].getDots() - 1]++;
-		}
-		for (let i = this.dice.length; i >= 0; i--) {
-			if (occurences[i] >= 2 && pair1 === undefined) {
-				pair1 = i + 1;
-			} else if (occurences[i] >= 2) {
-				pair2 = i + 1;
-				return pair1 * 2 + pair2 * 2;
-			}
-		}
-		return 0;
-	}
+    calcFirstHalf() {
+        var firstHalfPoints = [];
+        var points = 0;
+        for (let i = 0; i < 6; i++) {
+            for (let j = 0; j < this.dice.length; j++) {
+                if (this.dice[j].getDots() == i + 1) {
+                    points += this.dice[j].getDots();
+                }
+            }
+            firstHalfPoints.push(points);
+            points = 0;
+        }
+        return firstHalfPoints;
+    }
 
-	checkStraight(smallStraight) {
-		var sortedDots = [];
-		for (let i = 0; i < this.dice.length; i++) {
-			sortedDots.push(this.dice[i].getDots());
-		}
-		sortedDots.sort();
+    checkOccurences(nbrOfOccurences) {
+        var points = 0;
+        var occurences = 0;
+        for (let i = 6; i > 0; i--) {
+            for (let j = 0; j < this.dice.length; j++) {
+                if (this.dice[j].getDots() == i) {
+                    occurences++;
+                }
+            }
+            if (occurences >= nbrOfOccurences) {
+                points = i * nbrOfOccurences;
+                break;
+            }
+            occurences = 0;
+        }
+        return points;
+    }
 
-		if (smallStraight) {
-			if (sortedDots[0] === 1 && sortedDots[1] === 2 && sortedDots[2] === 3 && sortedDots[3] === 4 && sortedDots[4] === 5) {
-				return 15;
-			}
-		} else {
-			if (sortedDots[0] === 2 && sortedDots[1] === 3 && sortedDots[2] === 4 && sortedDots[3] === 5 && sortedDots[4] === 6) {
-				return 20;
-			}
-		}
-		return 0;
-	}
+    checkDoublePair() {
+        var occurences = [0, 0, 0, 0, 0, 0];
+        var pair1, pair2;
+        for (let i = 0; i < this.dice.length; i++) {
+            occurences[this.dice[i].getDots() - 1]++;
+        }
+        for (let i = this.dice.length; i >= 0; i--) {
+            if (occurences[i] >= 2 && pair1 === undefined) {
+                pair1 = i + 1;
+            } else if (occurences[i] >= 2) {
+                pair2 = i + 1;
+                return pair1 * 2 + pair2 * 2;
+            }
+        }
+        return 0;
+    }
 
-	checkFullHouse() {
-		var occurences = [0, 0, 0, 0, 0, 0];
-		var threes, pair;
-		for (let i = 0; i < this.dice.length; i++) {
-			occurences[this.dice[i].getDots() - 1]++;
-		}
+    checkStraight(smallStraight) {
+        var sortedDots = [];
+        for (let i = 0; i < this.dice.length; i++) {
+            sortedDots.push(this.dice[i].getDots());
+        }
+        sortedDots.sort();
 
-		for (let i = this.dice.length; i >= 0; i--) {
-			if (occurences[i] >= 3) {
-				threes = i + 1;
-			}
-		}
+        if (smallStraight) {
+            if (sortedDots[0] === 1 && sortedDots[1] === 2 && sortedDots[2] === 3 && sortedDots[3] === 4 && sortedDots[4] === 5) {
+                return 15;
+            }
+        } else {
+            if (sortedDots[0] === 2 && sortedDots[1] === 3 && sortedDots[2] === 4 && sortedDots[3] === 5 && sortedDots[4] === 6) {
+                return 20;
+            }
+        }
+        return 0;
+    }
 
-		for (let i = this.dice.length; i >= 0; i--) {
-			if (occurences[i] >= 3) {
-				threes = i + 1;
-			}
-		}
+    checkFullHouse() {
+        var occurences = [0, 0, 0, 0, 0, 0];
+        var threes, pair;
+        for (let i = 0; i < this.dice.length; i++) {
+            occurences[this.dice[i].getDots() - 1]++;
+        }
 
-		for (let i = this.dice.length; i >= 0; i--) {
-			if (threes && occurences[i] >= 2 && i != threes - 1) {
-				pair = i + 1;
-				console.log('Threes:', threes, 'Pair:	', pair);
-				return threes * 3 + pair * 2;
-			}
-		}
+        for (let i = this.dice.length; i >= 0; i--) {
+            if (occurences[i] >= 3) {
+                threes = i + 1;
+            }
+        }
 
-		return 0;
-	}
+        for (let i = this.dice.length; i >= 0; i--) {
+            if (occurences[i] >= 3) {
+                threes = i + 1;
+            }
+        }
 
-	checkYatzy() {
-		var yatzy = false;
-		var occurences = 0;
-		for (let i = 6; i > 0; i--) {
-			for (let j = 0; j < this.dice.length; j++) {
-				if (this.dice[j].getDots() == i) {
-					occurences++;
-				}
-			}
-			if (occurences === 5) {
-				return 50;
-			}
-			occurences = 0;
-		}
-		return 0;
-	}
+        for (let i = this.dice.length; i >= 0; i--) {
+            if (threes && occurences[i] >= 2 && i != threes - 1) {
+                pair = i + 1;
+                console.log('Threes:', threes, 'Pair:	', pair);
+                return threes * 3 + pair * 2;
+            }
+        }
 
-	calcChance() {
-		var points = 0;
-		for (let i = 0; i < this.dice.length; i++) {
-			points += this.dice[i].getDots();
-		}
-		return points;
-	}
+        return 0;
+    }
 
-	switchPlayer() {
-		if(this.currentPlayer + 1 === this.players.length) {
-			this.currentPlayer = 0;
-		} else {
-			this.currentPlayer++;
-		}
-	}
-	
-	scorePlayers(id, userName, points) {
-		var testArr = [1, 1337, 250];
-		this.db.scorePlayers({
+    checkYatzy() {
+        var yatzy = false;
+        var occurences = 0;
+        for (let i = 6; i > 0; i--) {
+            for (let j = 0; j < this.dice.length; j++) {
+                if (this.dice[j].getDots() == i) {
+                    occurences++;
+                }
+            }
+            if (occurences === 5) {
+                return 50;
+            }
+            occurences = 0;
+        }
+        return 0;
+    }
 
-			id: testArr[0],
-			userName: testArr[1],
-			points: testArr[2]
+    calcChance() {
+        var points = 0;
+        for (let i = 0; i < this.dice.length; i++) {
+            points += this.dice[i].getDots();
+        }
+        return points;
+    }
 
-		})
-	}
+    switchPlayer() {
+        if (this.currentPlayer + 1 === this.players.length) {
+            this.currentPlayer = 0;
+        } else {
+            this.currentPlayer++;
+        }
+    }
 
-	scoreGames(id, date) {
-		this.db.scoreGames({
+    scorePlayers(id, userName, points) {
+        var testArr = [1, 1337, 250];
+        this.db.scorePlayers({
 
-			id: id,
-			date: date
-		})
-	}
+            id: testArr[0],
+            userName: testArr[1],
+            points: testArr[2]
 
-	gamesHasPlayers(games_id, players_id) {
-		this.db.gamesHasPlayers({
-			games_id: games_id,
-			players_id: players_id
-		})
-	}
-	static get sqlQueries() {
-		return {
-			scorePlayers: ` 
+        })
+    }
+
+    scoreGames(id, date) {
+        this.db.scoreGames({
+
+            id: id,
+            date: date
+        })
+    }
+
+    gamesHasPlayers(games_id, players_id) {
+        this.db.gamesHasPlayers({
+            games_id: games_id,
+            players_id: players_id
+        })
+    }
+    static get sqlQueries() {
+        return {
+            scorePlayers: ` 
      INSERT players SET ? 
      `,
-			scoreGames: `INSERT games SET ?`,
+            scoreGames: `INSERT games SET ?`,
 
-			gamesHasPlayers: `INSERT games_has_players SET ?`
-		}
-	}
+            gamesHasPlayers: `INSERT games_has_players SET ?`
+        }
+    }
 
 }
--- js/classes/protocol.class.js
+++ js/classes/protocol.class.js
@@ -1,58 +1,57 @@
 class Protocol extends Base {
 
-	constructor(propertyValues) {
-		super(propertyValues);
-	}
+    constructor(propertyValues) {
+        super(propertyValues);
+    }
 
-	createColumn() {
-		var tempPlayers = this.players;
-		let j = 0;
-		for (let i = 0; i < this.players.length; i++) {
-			$('table').find('thead').each(function () {
-				$(this).find('th').last().after('<th>' + tempPlayers[i] + '</th>');
-			});
+    createColumn() {
+        var tempPlayers = this.players;
+        let j = 0;
+        for (let i = 0; i < this.players.length; i++) {
+            $('table').find('thead').each(function () {
+                $(this).find('th').last().after('<th>' + tempPlayers[i] + '</th>');
+            });
 
-			$('table').find('tr').each(function () {
-				$(this).find('td').last().after('<td class="' + tempPlayers[i] + ' ' + j + '"></td>');
-				j++;
-			});
-		}
-	}
+            $('table').find('tr').each(function () {
+                $(this).find('td').last().after('<td class="' + tempPlayers[i] + ' ' + j + '"></td>');
+                j++;
+            });
+        }
+    }
 
-	insertPotentialPoints(points, currentPlayer) {
-		let tempPlayers = this.players;
-		let protocol = this;
-		this.currentPlayer = currentPlayer;
+    insertPotentialPoints(points, currentPlayer) {
+        let tempPlayers = this.players;
+        let protocol = this;
+        this.currentPlayer = currentPlayer;
 
-		let i = 0;
-		$('.' + tempPlayers[currentPlayer]).each(function () {
-				console.log('debug 1', $(this).html());
-				console.log('debug 2', $(this + ':contains("<a")');
-					if ($(this + ':contains("<a")') {
-							console.log('tjo');
-							$(this).empty();
-							var potentialPoint = new PotentialPoint({
-								id: $(this).attr("class").split(' ')[1],
-								points: points[i]
-							});
-							potentialPoint.protocol = protocol;
-							potentialPoint.display(this);
-							i++;
-						}
-					});
-			}
+        let i = 0;
+        $('.' + tempPlayers[currentPlayer]).each(function () {
+            console.log('debug 1', $(this).html());
+            console.log('debug 2', $(this) + ':contains("<a")');
+            if ($(this) + ':contains("<a")') {
+                console.log('tjo');
+                $(this).empty();
+                var potentialPoint = new PotentialPoint({
+                    id: $(this).attr("class").split(' ')[1],
+                    points: points[i]
+                });
+                potentialPoint.protocol = protocol;
+                potentialPoint.display(this);
+                i++;
+            }
+        });
+    }
 
-			selectPoint(point) {
-				let tempPlayers = this.players;
-				let currentPlayer = this.currentPlayer;
-				$('.' + tempPlayers[currentPlayer]).each(function () {
-					if (point.id === $(this).attr("class").split(' ')[1]) {
-						console.log(point.points);
-						$(this).empty();
-						$(this).append(point.points);
-					}
-				});
-			}
+    selectPoint(point) {
+        let tempPlayers = this.players;
+        let currentPlayer = this.currentPlayer;
+        $('.' + tempPlayers[currentPlayer]).each(function () {
+            if (point.id === $(this).attr("class").split(' ')[1]) {
+                console.log(point.points);
+                $(this).empty();
+                $(this).append(point.points);
+            }
+        });
+    }
 
-		}
-	}
+}
