525c51577218b40f07cdd2d52f8bfea116ccdc37
Filip <tjo>
2017-04-05 12:52:30 +0200

    selected points no longer change after re-rolling dice

--- js/classes/protocol.class.js
+++ js/classes/protocol.class.js
@@ -1,58 +1,58 @@
 class Protocol extends Base {
 
-    constructor(propertyValues) {
-        super(propertyValues);
-    }
+	constructor(propertyValues) {
+		super(propertyValues);
+	}
 
-    createColumn() {
-        var tempPlayers = this.players;
-        let j = 0;
-        for (let i = 0; i < this.players.length; i++) {
-            $('table').find('thead').each(function () {
-                $(this).find('th').last().after('<th>' + tempPlayers[i] + '</th>');
-            });
+	createColumn() {
+		var tempPlayers = this.players;
+		let j = 0;
+		for (let i = 0; i < this.players.length; i++) {
+			$('table').find('thead').each(function () {
+				$(this).find('th').last().after('<th>' + tempPlayers[i] + '</th>');
+			});
 
-            $('table').find('tr').each(function () {
-                $(this).find('td').last().after('<td class="' + tempPlayers[i] + ' ' + j + '"></td>');
-                j++;
-            });
-        }
-    }
+			$('table').find('tr').each(function () {
+				$(this).find('td').last().after('<td class="' + tempPlayers[i] + ' ' + j + '"  locked=false></td>');
+				j++;
+			});
+		}
+	}
 
-    insertPotentialPoints(points, currentPlayer) {
-        let tempPlayers = this.players;
-        let protocol = this;
-        this.currentPlayer = currentPlayer;
+	insertPotentialPoints(points, currentPlayer) {
+		let tempPlayers = this.players;
+		let protocol = this;
+		this.currentPlayer = currentPlayer;
 
-        let i = 0;
-        $('.' + tempPlayers[currentPlayer]).each(function () {
-            console.log('debug 1', $(this).html());
-            console.log('debug 2', $(this) + ':contains("<a")');
-            if ($(this) + ':contains("<a")') {
-                console.log('tjo');
-                $(this).empty();
-                var potentialPoint = new PotentialPoint({
-                    id: $(this).attr("class").split(' ')[1],
-                    points: points[i]
-                });
-                potentialPoint.protocol = protocol;
-                potentialPoint.display(this);
-                i++;
-            }
-        });
-    }
+		let i = 0;
+		$('.' + tempPlayers[currentPlayer]).each(function () {
+			console.log('debug 1', $(this).attr('locked'));
+			console.log('debug 2', $(this));
+			if ($(this).attr('locked') == 'false') {
+				$(this).empty();
+				var potentialPoint = new PotentialPoint({
+					id: $(this).attr("class").split(' ')[1],
+					points: points[i]
+				});
+				potentialPoint.protocol = protocol;
+				potentialPoint.display(this);
+				i++;
+			}
+		});
+	}
 
-    selectPoint(point) {
-        let tempPlayers = this.players;
-        let currentPlayer = this.currentPlayer;
-        $('.' + tempPlayers[currentPlayer]).each(function () {
-            if (point.id === $(this).attr("class").split(' ')[1]) {
-                console.log(point.points);
-                $(this).empty();
-                $(this).append(point.points);
-                $(this).addClass('success');
-            }
-        });
-    }
+	selectPoint(point) {
+		let tempPlayers = this.players;
+		let currentPlayer = this.currentPlayer;
+		$('.' + tempPlayers[currentPlayer]).each(function () {
+			if (point.id === $(this).attr("class").split(' ')[1]) {
+				console.log(point.points);
+				$(this).empty();
+				$(this).append(point.points);
+				$(this).addClass('success');
+				$(this).attr('locked', 'true');
+			}
+		});
+	}
 
 }
