fb101bc835dd5d26638ee17fef6ad927169ccad9
Filip <tjo>
2017-04-06 20:19:00 +0200

    Melded and welded sum-code from different branches

--- js/classes/game-board.class.js
+++ js/classes/game-board.class.js
@@ -1,296 +1,296 @@
-class GameBoard extends Base {
+	class GameBoard extends Base {
 
-	constructor(propertyValues) {
-		super(propertyValues);
-		this.currentPlayer = 0;
-		this.turns = 0; //A global variable to calculate the number of tens and check if they are three
-	}
-
-	createDice() {
-		this.dice = [];
-		for (let i = 0; i < 5; i++) {
-			let die = new Die({
-				id: i + 1
-			});
-			die.display('#dice');
-			this.dice.push(die);
-			die.resetDie();
-		}
-	}
-
-	rollTheDice() {
-		for (let i = 0; i < this.dice.length; i++) {
-			this.dice[i].rollTheDice();
-		}
-		var points = this.calcPotentialPoints();
-		this.protocol.insertPotentialPoints(points, this.currentPlayer);
-		this.turns++;
-
-		if (this.turns === 3) {
-			$('.btn').prop('disabled', true);
-		}
-	}
-
-	createProtocol() {
-		var protocol = new Protocol({
-			players: this.players,
-			gameboard: this
-		});
-		this.protocol = protocol;
-		protocol.display('#protocolContainer');
-		protocol.createColumn();
-	}
-
-	calcPotentialPoints() {
-		var points = [];
-		points = points.concat(this.calcFirstHalf());
-		points.push(this.checkOccurences(2));
-		points.push(this.checkDoublePair());
-		points.push(this.checkOccurences(3));
-		points.push(this.checkOccurences(4));
-		points.push(this.checkStraight(true));
-		points.push(this.checkStraight(false));
-		points.push(this.checkFullHouse());
-		points.push(this.calcChance());
-		points.push(this.checkYatzy());
-
-		let sumFirstHalf = this.summerizeFirstHalf();
-		points.splice(6, 0, 0);
-		points.splice(7, 0, 0);
-		points.push(0);
-
-		console.log('Points: ', points);
-
-		return points;
-	}
-
-	calcFirstHalf() {
-		var firstHalfPoints = [];
-		var points = 0;
-		for (let i = 0; i < 6; i++) {
-			for (let j = 0; j < this.dice.length; j++) {
-				if (this.dice[j].getDots() == i + 1) {
-					points += this.dice[j].getDots();
-				}
-			}
-			firstHalfPoints.push(points);
-			points = 0;
-		}
-		return firstHalfPoints;
-	}
-
-	checkOccurences(nbrOfOccurences) {
-		var points = 0;
-		var occurences = 0;
-		for (let i = 6; i > 0; i--) {
-			for (let j = 0; j < this.dice.length; j++) {
-				if (this.dice[j].getDots() == i) {
-					occurences++;
-				}
-			}
-			if (occurences >= nbrOfOccurences) {
-				points = i * nbrOfOccurences;
-				break;
-			}
-			occurences = 0;
-		}
-		return points;
-	}
-
-	checkDoublePair() {
-		var occurences = [0, 0, 0, 0, 0, 0];
-		var pair1, pair2;
-		for (let i = 0; i < this.dice.length; i++) {
-			occurences[this.dice[i].getDots() - 1]++;
-		}
-		for (let i = this.dice.length; i >= 0; i--) {
-			if (occurences[i] >= 2 && pair1 === undefined) {
-				pair1 = i + 1;
-			} else if (occurences[i] >= 2) {
-				pair2 = i + 1;
-				return pair1 * 2 + pair2 * 2;
-			}
-		}
-		return 0;
-	}
-
-	checkStraight(smallStraight) {
-		var sortedDots = [];
-		for (let i = 0; i < this.dice.length; i++) {
-			sortedDots.push(this.dice[i].getDots());
-		}
-		sortedDots.sort();
-
-		if (smallStraight) {
-			if (sortedDots[0] === 1 && sortedDots[1] === 2 && sortedDots[2] === 3 && sortedDots[3] === 4 && sortedDots[4] === 5) {
-				return 15;
-			}
-		} else {
-			if (sortedDots[0] === 2 && sortedDots[1] === 3 && sortedDots[2] === 4 && sortedDots[3] === 5 && sortedDots[4] === 6) {
-				return 20;
-			}
-		}
-		return 0;
-	}
-
-	checkFullHouse() {
-		var occurences = [0, 0, 0, 0, 0, 0];
-		var threes, pair;
-		for (let i = 0; i < this.dice.length; i++) {
-			occurences[this.dice[i].getDots() - 1]++;
-		}
-
-		for (let i = this.dice.length; i >= 0; i--) {
-			if (occurences[i] >= 3) {
-				threes = i + 1;
-			}
-		}
-
-		for (let i = this.dice.length; i >= 0; i--) {
-			if (occurences[i] >= 3) {
-				threes = i + 1;
-			}
-		}
-
-		for (let i = this.dice.length; i >= 0; i--) {
-			if (threes && occurences[i] >= 2 && i != threes - 1) {
-				pair = i + 1;
-				console.log('Threes:', threes, 'Pair:	', pair);
-				return threes * 3 + pair * 2;
-			}
-		}
-
-		return 0;
-	}
-
-	checkYatzy() {
-		var yatzy = false;
-		var occurences = 0;
-		for (let i = 6; i > 0; i--) {
-			for (let j = 0; j < this.dice.length; j++) {
-				if (this.dice[j].getDots() == i) {
-					occurences++;
-				}
-			}
-			if (occurences === 5) {
-				return 50;
-			}
-			occurences = 0;
-		}
-		return 0;
-	}
-
-	calcChance() {
-		var points = 0;
-		for (let i = 0; i < this.dice.length; i++) {
-			points += this.dice[i].getDots();
-		}
-		return points;
-	}
-
-	summerizeFirstHalf() {
-		let sum = 0;
-		let bonus = 0;
-		let i = 0;
-		$('.' + this.players[this.currentPlayer]).each(function () {
-			if (i > 6) {
-				return false;
-			}
-			if ($(this).attr('locked') == 'true') {
-				sum += parseInt($(this).text());
-			}
-			i++;
-		});
-
-		if (sum >= 63) {
-			bonus = 50;
-		}
-
-		return [sum, bonus];
-	}
-
-	summerizeColumn() {
-		let sum = 0;
-		let i = 0;
-		$('.' + this.players[this.currentPlayer]).each(function () {
-			if (i !== 6 && i !== 17 && $(this).attr('locked') == 'true') {
-				sum += parseInt($(this).text());
-			}
-			i++;
-		});
-		return sum;
-	}
-
-	switchPlayer() {
-		this.turns = 0;
-		$('.btn').prop('disabled', false);
-
-		let pointsFirstHalf = this.summerizeFirstHalf();
-		$('.' + this.players[this.currentPlayer] + '.7').empty();
-		$('.' + this.players[this.currentPlayer] + '.7').append(pointsFirstHalf[0]);
-		$('.' + this.players[this.currentPlayer] + '.8').empty();
-		$('.' + this.players[this.currentPlayer] + '.8').append(pointsFirstHalf[1]);
-		
-		$('.' + this.players[this.currentPlayer] + '.18').empty();
-		$('.' + this.players[this.currentPlayer] + '.18').append(this.summerizeColumn());
-		
-		
-
-		for (let i = 0; i < this.dice.length; i++) {
-			this.dice[i].resetDie();
-		}
-
-		if (this.currentPlayer + 1 === this.players.length) {
+		constructor(propertyValues) {
+			super(propertyValues);
 			this.currentPlayer = 0;
-		} else {
-			this.currentPlayer++;
+			this.turns = 0; //A global variable to calculate the number of tens and check if they are three
 		}
 
-	}
+		createDice() {
+			this.dice = [];
+			for (let i = 0; i < 5; i++) {
+				let die = new Die({
+					id: i + 1
+				});
+				die.display('#dice');
+				this.dice.push(die);
+				die.resetDie();
+			}
+		}
 
-	scorePlayers(id, userName, points) {
-		var testArr = [1, 1337, 250];
-		this.db.scorePlayers({
+		rollTheDice() {
+			for (let i = 0; i < this.dice.length; i++) {
+				this.dice[i].rollTheDice();
+			}
+			var points = this.calcPotentialPoints();
+			this.protocol.insertPotentialPoints(points, this.currentPlayer);
+			this.turns++;
 
-			id: testArr[0],
-			userName: testArr[1],
-			points: testArr[2]
+			if (this.turns === 3) {
+				$('.btn').prop('disabled', true);
+			}
+		}
 
-		})
-	}
+		createProtocol() {
+			var protocol = new Protocol({
+				players: this.players,
+				gameboard: this
+			});
+			this.protocol = protocol;
+			protocol.display('#protocolContainer');
+			protocol.createColumn();
+		}
 
-	scoreGames(id, date) {
-		this.db.scoreGames({
-			id: id,
-			date: date
-		})
-	}
+		calcPotentialPoints() {
+			var points = [];
+			points = points.concat(this.calcFirstHalf());
+			points.push(this.checkOccurences(2));
+			points.push(this.checkDoublePair());
+			points.push(this.checkOccurences(3));
+			points.push(this.checkOccurences(4));
+			points.push(this.checkStraight(true));
+			points.push(this.checkStraight(false));
+			points.push(this.checkFullHouse());
+			points.push(this.calcChance());
+			points.push(this.checkYatzy());
 
-	scoreEndGames(userName, point) {
-		var endArr = [];
-		endArr.push(userName);
-		endArr.push(point);
-		console.log(endArr);
-	}
+			let sumFirstHalf = this.summerizeFirstHalf();
+			points.splice(6, 0, 0);
+			points.splice(7, 0, 0);
+			points.push(0);
 
-	gamesHasPlayers(games_id, players_id) {
-		this.db.gamesHasPlayers({
-			games_id: games_id,
-			players_id: players_id
-		})
-	}
+			console.log('Points: ', points);
 
-	static get sqlQueries() {
-		return {
-			scorePlayers: `
+			return points;
+		}
+
+		calcFirstHalf() {
+			var firstHalfPoints = [];
+			var points = 0;
+			for (let i = 0; i < 6; i++) {
+				for (let j = 0; j < this.dice.length; j++) {
+					if (this.dice[j].getDots() == i + 1) {
+						points += this.dice[j].getDots();
+					}
+				}
+				firstHalfPoints.push(points);
+				points = 0;
+			}
+			return firstHalfPoints;
+		}
+
+		checkOccurences(nbrOfOccurences) {
+			var points = 0;
+			var occurences = 0;
+			for (let i = 6; i > 0; i--) {
+				for (let j = 0; j < this.dice.length; j++) {
+					if (this.dice[j].getDots() == i) {
+						occurences++;
+					}
+				}
+				if (occurences >= nbrOfOccurences) {
+					points = i * nbrOfOccurences;
+					break;
+				}
+				occurences = 0;
+			}
+			return points;
+		}
+
+		checkDoublePair() {
+			var occurences = [0, 0, 0, 0, 0, 0];
+			var pair1, pair2;
+			for (let i = 0; i < this.dice.length; i++) {
+				occurences[this.dice[i].getDots() - 1]++;
+			}
+			for (let i = this.dice.length; i >= 0; i--) {
+				if (occurences[i] >= 2 && pair1 === undefined) {
+					pair1 = i + 1;
+				} else if (occurences[i] >= 2) {
+					pair2 = i + 1;
+					return pair1 * 2 + pair2 * 2;
+				}
+			}
+			return 0;
+		}
+
+		checkStraight(smallStraight) {
+			var sortedDots = [];
+			for (let i = 0; i < this.dice.length; i++) {
+				sortedDots.push(this.dice[i].getDots());
+			}
+			sortedDots.sort();
+
+			if (smallStraight) {
+				if (sortedDots[0] === 1 && sortedDots[1] === 2 && sortedDots[2] === 3 && sortedDots[3] === 4 && sortedDots[4] === 5) {
+					return 15;
+				}
+			} else {
+				if (sortedDots[0] === 2 && sortedDots[1] === 3 && sortedDots[2] === 4 && sortedDots[3] === 5 && sortedDots[4] === 6) {
+					return 20;
+				}
+			}
+			return 0;
+		}
+
+		checkFullHouse() {
+			var occurences = [0, 0, 0, 0, 0, 0];
+			var threes, pair;
+			for (let i = 0; i < this.dice.length; i++) {
+				occurences[this.dice[i].getDots() - 1]++;
+			}
+
+			for (let i = this.dice.length; i >= 0; i--) {
+				if (occurences[i] >= 3) {
+					threes = i + 1;
+				}
+			}
+
+			for (let i = this.dice.length; i >= 0; i--) {
+				if (occurences[i] >= 3) {
+					threes = i + 1;
+				}
+			}
+
+			for (let i = this.dice.length; i >= 0; i--) {
+				if (threes && occurences[i] >= 2 && i != threes - 1) {
+					pair = i + 1;
+					console.log('Threes:', threes, 'Pair:	', pair);
+					return threes * 3 + pair * 2;
+				}
+			}
+
+			return 0;
+		}
+
+		checkYatzy() {
+			var yatzy = false;
+			var occurences = 0;
+			for (let i = 6; i > 0; i--) {
+				for (let j = 0; j < this.dice.length; j++) {
+					if (this.dice[j].getDots() == i) {
+						occurences++;
+					}
+				}
+				if (occurences === 5) {
+					return 50;
+				}
+				occurences = 0;
+			}
+			return 0;
+		}
+
+		calcChance() {
+			var points = 0;
+			for (let i = 0; i < this.dice.length; i++) {
+				points += this.dice[i].getDots();
+			}
+			return points;
+		}
+
+		summerizeFirstHalf() {
+			let sum = 0;
+			let bonus = 0;
+			let i = 0;
+			$('.' + this.players[this.currentPlayer]).each(function () {
+				if (i > 6) {
+					return false;
+				}
+				if ($(this).attr('locked') == 'true') {
+					sum += parseInt($(this).text());
+				}
+				i++;
+			});
+
+			if (sum >= 63) {
+				bonus = 50;
+			}
+
+			return [sum, bonus];
+		}
+
+		summerizeColumn() {
+			let sum = 0;
+			let i = 0;
+			$('.' + this.players[this.currentPlayer]).each(function () {
+				if (i !== 6 && i !== 17 && $(this).attr('locked') == 'true') {
+					sum += parseInt($(this).text());
+				}
+				i++;
+			});
+			return sum;
+		}
+
+		switchPlayer() {
+			this.turns = 0;
+			$('.btn').prop('disabled', false);
+
+			let pointsFirstHalf = this.summerizeFirstHalf();
+			$('.' + this.players[this.currentPlayer] + '.7').empty();
+			$('.' + this.players[this.currentPlayer] + '.7').append(pointsFirstHalf[0]);
+			$('.' + this.players[this.currentPlayer] + '.8').empty();
+			$('.' + this.players[this.currentPlayer] + '.8').append(pointsFirstHalf[1]);
+
+			$('.' + this.players[this.currentPlayer] + '.18').empty();
+			$('.' + this.players[this.currentPlayer] + '.18').append(this.summerizeColumn());
+
+
+
+			for (let i = 0; i < this.dice.length; i++) {
+				this.dice[i].resetDie();
+			}
+
+			if (this.currentPlayer + 1 === this.players.length) {
+				this.currentPlayer = 0;
+			} else {
+				this.currentPlayer++;
+			}
+
+		}
+
+		scorePlayers(id, userName, points) {
+			var testArr = [1, 1337, 250];
+			this.db.scorePlayers({
+
+				id: testArr[0],
+				userName: testArr[1],
+				points: testArr[2]
+
+			})
+		}
+
+		scoreGames(id, date) {
+			this.db.scoreGames({
+				id: id,
+				date: date
+			})
+		}
+
+		scoreEndGames(userName, point) {
+			var endArr = [];
+			endArr.push(userName);
+			endArr.push(point);
+			console.log(endArr);
+		}
+
+		gamesHasPlayers(games_id, players_id) {
+			this.db.gamesHasPlayers({
+				games_id: games_id,
+				players_id: players_id
+			})
+		}
+
+		static get sqlQueries() {
+			return {
+				scorePlayers: `
 				INSERT players SET ?
 				`,
-			scoreGames: `
+				scoreGames: `
 				INSERT games SET ? `,
 
-			gamesHasPlayers: `
+				gamesHasPlayers: `
 				INSERT games_has_players SET ? `
+			}
 		}
-	}
 
-}
+	}
--- js/classes/protocol.class.js
+++ js/classes/protocol.class.js
@@ -1,77 +1,77 @@
-class Protocol extends Base {
+	class Protocol extends Base {
 
-	constructor(propertyValues) {
-		super(propertyValues);
-	}
+		constructor(propertyValues) {
+			super(propertyValues);
+		}
 
-	createColumn() {
-		var tempPlayers = this.players;
-		for (let i = 0; i < this.players.length; i++) {
-			$('table').find('thead').each(function () {
-				$(this).find('th').last().after('<th>' + tempPlayers[i] + '</th>');
-			});
+		createColumn() {
+			var tempPlayers = this.players;
+			for (let i = 0; i < this.players.length; i++) {
+				$('table').find('thead').each(function () {
+					$(this).find('th').last().after('<th>' + tempPlayers[i] + '</th>');
+				});
 
-			let j = 0;
-			$('table').find('tr').each(function () {
-				if (j !== 7 && j !== 8 && j !== 18) {
-					$(this).find('td').last().after('<td class="' + tempPlayers[i] + ' ' + j + ' green"  locked=false></td>');
-				} else {
-					console.log('debug 1');
-					$(this).find('td').last().after('<td class="' + tempPlayers[i] + ' ' + j + ' bold"  locked=false>0</td>');
+				let j = 0;
+				$('table').find('tr').each(function () {
+					if (j !== 7 && j !== 8 && j !== 18) {
+						$(this).find('td').last().after('<td class="' + tempPlayers[i] + ' ' + j + ' green"  locked=false></td>');
+					} else {
+						console.log('debug 1');
+						$(this).find('td').last().after('<td class="' + tempPlayers[i] + ' ' + j + ' bold"  locked=false>0</td>');
+					}
+					j++;
+				});
+			}
+		}
+
+		insertPotentialPoints(points, currentPlayer) {
+			let tempPlayers = this.players;
+			let protocol = this;
+			this.currentPlayer = currentPlayer;
+			let gameboard = this.gameboard;
+
+			let i = 0;
+			$('.' + tempPlayers[currentPlayer]).each(function () {
+				if ($(this).attr('locked') == 'false' && i !== 6 && i !== 17 && i !== 7) {
+					$(this).empty();
+					var potentialPoint = new PotentialPoint({
+						id: $(this).attr("class").split(' ')[1],
+						points: points[i]
+					});
+					potentialPoint.protocol = protocol;
+					potentialPoint.display(this);
 				}
-				j++;
+				i++;
 			});
 		}
+
+		selectPoint(point) {
+			let tempPlayers = this.players;
+			let currentPlayer = this.currentPlayer;
+			$('.' + tempPlayers[currentPlayer]).each(function () {
+				if (point.id === $(this).attr("class").split(' ')[1]) {
+					console.log(point.points);
+					$(this).empty();
+					$(this).append(point.points);
+					$(this).addClass('success');
+					$(this).attr('locked', 'true');
+				}
+			});
+			this.clearPotentialPoints();
+			this.gameboard.switchPlayer();
+		}
+
+		clearPotentialPoints() {
+			let tempPlayers = this.players;
+			let currentPlayer = this.currentPlayer;
+			let i = 0;
+
+			$('.' + tempPlayers[currentPlayer]).each(function () {
+				if ($(this).attr('locked') == 'false' && i !== 6 && i !== 17 && i !== 7) {
+					$(this).empty();
+				}
+				i++;
+			});
+		}
+
 	}
-
-	insertPotentialPoints(points, currentPlayer) {
-		let tempPlayers = this.players;
-		let protocol = this;
-		this.currentPlayer = currentPlayer;
-		let gameboard = this.gameboard;
-
-		let i = 0;
-		$('.' + tempPlayers[currentPlayer]).each(function () {
-			if ($(this).attr('locked') == 'false' && i !== 6 && i !== 17 && i !== 7) {
-				$(this).empty();
-				var potentialPoint = new PotentialPoint({
-					id: $(this).attr("class").split(' ')[1],
-					points: points[i]
-				});
-				potentialPoint.protocol = protocol;
-				potentialPoint.display(this);
-			}
-			i++;
-		});
-	}
-
-	selectPoint(point) {
-		let tempPlayers = this.players;
-		let currentPlayer = this.currentPlayer;
-		$('.' + tempPlayers[currentPlayer]).each(function () {
-			if (point.id === $(this).attr("class").split(' ')[1]) {
-				console.log(point.points);
-				$(this).empty();
-				$(this).append(point.points);
-				$(this).addClass('success');
-				$(this).attr('locked', 'true');
-			}
-		});
-		this.clearPotentialPoints();
-		this.gameboard.switchPlayer();
-	}
-
-	clearPotentialPoints() {
-		let tempPlayers = this.players;
-		let currentPlayer = this.currentPlayer;
-		let i = 0;
-
-		$('.' + tempPlayers[currentPlayer]).each(function () {
-			if ($(this).attr('locked') == 'false' && i !== 6 && i !== 17 && i !== 7) {
-				$(this).empty();
-			}
-			i++;
-		});
-	}
-
-}
