59446feabae2989218515780a3309281452e6a8f
Filip <tjo>
2017-04-08 11:44:53 +0200

    added leaderboard in statistics changed the way statistics is displayed

--- js/classes/statistics.class.js
+++ js/classes/statistics.class.js
@@ -5,13 +5,36 @@ class Statistics extends Base {
 
 		var playerResultList = new PlayerResultList();
 		playerResultList.readPlayerResult(() => {
-			playerResultList.display('#playerResult')
-		});
+			playerResultList.display('#playerResult');
 
+			var gamesResultList = new GamesResultList();
+			gamesResultList.readGamesResult(() => {
+				let games = [];
+				for (let i = 0; i < gamesResultList.length; i++) {
+					let found = false;
+					for (let j = 0; j < games.length; j++) {
+						if (gamesResultList[i].id === games[j]) {
+							found = true;
+						}
+					}
+					if (!found) {
+						games.push(gamesResultList[i].id);
+					}
+				}
 
-		var gamesResultList = new GamesResultList();
-		gamesResultList.readGamesResult(() => {
-			gamesResultList.display('#gamesResult')
+				console.log('unsorted: ', games);
+				games = games.sort(function (a, b) {
+					return b - a;
+				});
+				console.log('sorted: ', games);
+				for (let i = 0; i < games.length; i++) {
+					$('#gamesResult').append('<div id="game' + games[i] + '"><h3>Game ' + games[i] + '</h3></div>');
+				}
+				for (let i = 0; i < gamesResultList.length; i++) {
+					gamesResultList[i].display('#game' + gamesResultList[i].id);
+				}
+			});
+
 		});
 
 	}
