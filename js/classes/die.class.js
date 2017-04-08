59ccad979b107083b0587b51f0b33c3e7a091648
Filip <tjo>
2017-04-08 19:32:05 +0200

    renamed css rules .dieDisabled -> .dieLocked .dieGrayed -> .dieDisabled

--- js/classes/die.class.js
+++ js/classes/die.class.js
@@ -2,17 +2,17 @@ class Die extends Base {
 
 	constructor(propertyValues) {
 		super(propertyValues);
-		this.enabled = true;
+		this.locked = true;
 		this.dots = "\u2680";
 		this.dieFaces = ["\u2680", "\u2681", "\u2682", "\u2683", "\u2684", "\u2685"];
 		this.counter = 0;
 	}
 
 	rollTheDie() {
-		$('.die' + this.id).removeClass("dieGrayed");
+		$('.die' + this.id).removeClass("dieDisabled");
 		
 		$('#errorMessage').remove(); //to delete the error message if the user rolls the dice so it is not shown anymore
-		if (this.enabled) {
+		if (this.locked) {
 			this.dots = Math.floor(Math.random() * 6 + 1);
 			$('#die' + this.id).html(this.dieFaces[this.dots - 1]);
 		}
@@ -27,17 +27,17 @@ class Die extends Base {
 		if (this.counter === 0) {
 			$('#errorMessage').remove();
 		} else {
-			this.enabled = !this.enabled;
-			$('.die' + this.id).toggleClass("dieDisabled");
+			this.locked = !this.locked;
+			$('.die' + this.id).toggleClass("dieLocked");
 		}
 	}
 	
 	resetDie() {
 		this.counter = 0;
-		$('.die' + this.id).removeClass("dieDisabled");
-		$('.die' + this.id).addClass("dieGrayed");
+		$('.die' + this.id).removeClass("dieLocked");
+		$('.die' + this.id).addClass("dieDisabled");
 		
-		this.enabled = true;
+		this.locked = true;
 	}
 
 }
